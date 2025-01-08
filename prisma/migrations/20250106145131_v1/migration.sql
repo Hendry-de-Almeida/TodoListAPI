2125ae92e2a0e5d9c754b77523928cd923d86250
Hendry-de-Almeida <hendryalmeida4@gmail.com>
2025-01-08 22:35:42 +0100

    Changed database to postgresql

--- prisma/migrations/20250106145131_v1/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "description" TEXT,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "completed_at" DATETIME,
-    "updated_at" DATETIME NOT NULL
-);
